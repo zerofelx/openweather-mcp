ae4ebf93d325452168219830ef5b18528aad820b
zerofelx <zeroflex3@gmail.com>
2025-04-22 07:09:16 -0500

    Trying to use this repository with npx

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env
+#!/usr/bin/env node
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
